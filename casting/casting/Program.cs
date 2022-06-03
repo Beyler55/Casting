ef3158583432efe3d1e9e3e710564290538371d1
Beyler55 <baylarss@code.edu.az>
2022-06-03 19:28:55 +0400

    casting task

---
+++ casting/casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Ekrem abi");
+        }
+        public static void Print(Object obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
